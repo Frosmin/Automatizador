a9d6b45e6cbb75a483ab0c1d1189232b9798d854
Frosmin <simonabastomart@gmail.com>
2024-06-13 19:34:26 -0400

    inicio de secion listo (sin tokens)

--- casa/urls.py
+++ casa/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from casa import views
 
 urlpatterns = [
-    path('', views.login),
+    path('', views.login_view),
+    path('home/', views.home, name='home'),
 ]
--- casa/views.py
+++ casa/views.py
@@ -1,9 +1,22 @@
-from django.shortcuts import render
+
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
-def login(request):
-    return render(request, 'login.html')
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')  #redirije si es user correcto
+        else:
+            return render(request, 'login.html', {'error': 'Usuario o contraseña no válidos'})
+    else:
+        return render(request, 'login.html')
+
