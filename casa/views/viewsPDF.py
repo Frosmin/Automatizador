047e8817b01f56d6e9231d1e271d5c74cd124416
Frosmin <simonabastomart@gmail.com>
2024-06-19 00:12:36 -0400

    mejora pdf

--- casa/views/viewsPDF.py
+++ casa/views/viewsPDF.py
@@ -1,31 +1,60 @@
 from django.http import FileResponse
-from reportlab.pdfgen import canvas
-from reportlab.lib.utils import ImageReader
 from casa.models.casaModel import Casa
 import io
+from reportlab.lib.pagesizes import letter
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image
+from reportlab.lib.styles import getSampleStyleSheet
+import lorem
 
 def some_view(request):
     # Crea un objeto de archivo PDF en memoria
     buffer = io.BytesIO()
-
     # Crea el PDF
-    p = canvas.Canvas(buffer)
+    doc = SimpleDocTemplate(buffer, pagesize=letter)
 
-    # Obtiene la imagen de la base de datos
-    # Esto es solo un ejemplo, necesitarás ajustarlo a tu modelo y método de obtención de la imagen
+    # Contenido del PDF
+    elements = []
+
+    # Estilos
+    styles = getSampleStyleSheet()
+
+    # Título
+    title = Paragraph("Título del Informe", styles['Title'])
+    elements.append(title)
+
+    # Espacio
+    elements.append(Spacer(1, 12))
+
+    # Subtítulo
+    subtitle = Paragraph("Subtítulo del Informe", styles['Heading2'])
+    elements.append(subtitle)
+
+    # Espacio
+    
+    elements.append(Spacer(1, 12))
+
+    lore = lorem.text()+ lorem.text()+lorem.text()+ lorem.text()+lorem.text()+ lorem.text()+lorem.text()+ lorem.text()+lorem.text()+ lorem.text()
+    # Texto
+    # text = Paragraph("""Aquí va el texto del informe..               
+    #                  """, styles['BodyText'])
+    
+    
+    
+    text = Paragraph(lore, styles['BodyText'])
+    elements.append(text)
+
+    # Espacio
+    elements.append(Spacer(1, 12))
+
+    # Imagen
     image_model = Casa.objects.get(id=1)
     image_path = image_model.foto
+    image = Image(io.BytesIO(image_path), 200, 200)
+    elements.append(image)
 
-    # Crea un ImageReader con la ruta de la imagen
-    image = ImageReader(io.BytesIO(image_path))
-
-    # Dibuja la imagen en el PDF (las coordenadas son x, y, ancho, alto)
-    p.drawImage(image, 100, 100, 200, 200)
-
-    p.drawString(100, 100, "Hello world.")
-    p.showPage()
-    p.save()
+    # Genera el PDF
+    doc.build(elements)
 
     # Crea una respuesta con el archivo PDF
     buffer.seek(0)
-    return FileResponse(buffer, as_attachment=True, filename='somefilename.pdf')
+    return FileResponse(buffer, as_attachment=True, filename='informe.pdf')
