80a396752f6bc307eaff8087b48a42f5b3e21107
Frosmin <simonabastomart@gmail.com>
2024-06-20 02:01:44 -0400

    mejorando informe

--- casa/views/viewsPDF.py
+++ casa/views/viewsPDF.py
@@ -1,4 +1,4 @@
-from django.http import FileResponse
+from django.http import FileResponse ,HttpResponse
 from casa.models.casaModel import Casa
 import io
 from reportlab.lib.pagesizes import letter
@@ -7,61 +7,79 @@ from reportlab.lib.styles import getSampleStyleSheet
 import lorem
 from django.http import FileResponse
 from casa.models.casaModel import Casa
-import io
-from reportlab.lib.pagesizes import letter
-from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image
-from reportlab.lib.styles import getSampleStyleSheet
-import lorem
+from django.shortcuts import redirect
+from django.views.decorators.csrf import csrf_exempt
 
+@csrf_exempt
 def some_view(request):
-    # Crea un objeto de archivo PDF en memoria
-    buffer = io.BytesIO()
-    # Crea el PDF
-    doc = SimpleDocTemplate(buffer, pagesize=letter)
-
-    # Contenido del PDF
-    elements = []
-
-    # Estilos
-    styles = getSampleStyleSheet()
-
-    # Título
-    title = Paragraph("Título del Informe", styles['Title'])
-    elements.append(title)
-
-    # Espacio
-    elements.append(Spacer(1, 12))
-
-    # Subtítulo
-    subtitle = Paragraph("Subtítulo del Informe", styles['Heading2'])
-    elements.append(subtitle)
-
-    # Espacio
-    
-    elements.append(Spacer(1, 12))
-
-    lore = lorem.text()+ lorem.text()+lorem.text()+ lorem.text()+lorem.text()+ lorem.text()+lorem.text()+ lorem.text()+lorem.text()+ lorem.text()
-    # Texto
-    # text = Paragraph("""Aquí va el texto del informe..               
-    #                  """, styles['BodyText'])
     
     
-    
-    text = Paragraph(lore, styles['BodyText'])
-    elements.append(text)
+    if request.method == 'POST':
+        numero_casa = request.POST.get('numero')
+        request.session['numero_casa'] = numero_casa
+        return redirect('some_view')
+    else:
+        numero_casa = request.session.get('numero_casa', None)
+        if numero_casa is None:
+            return HttpResponse('No se proporcionó un número de casa')
+        casa = Casa.objects.filter(numero=numero_casa).first()
+        if casa is None:
+            return HttpResponse('No existe una casa con ese número')
 
-    # Espacio
-    elements.append(Spacer(1, 12))
+        
+        
+        
+        # Crea un objeto de archivo PDF en memoria
+        buffer = io.BytesIO()
+        # Crea el PDF
+        doc = SimpleDocTemplate(buffer, pagesize=letter)
 
-    # Imagen
-    image_model = Casa.objects.get(id=1)
-    image_path = image_model.foto
-    image = Image(io.BytesIO(image_path), 200, 200)
-    elements.append(image)
+        # Contenido del PDF
+        elements = []
 
-    # Genera el PDF
-    doc.build(elements)
+        # Estilos
+        styles = getSampleStyleSheet()
 
-    # Crea una respuesta con el archivo PDF
-    buffer.seek(0)
-    return FileResponse(buffer, as_attachment=True, filename='informe.pdf')
+        # Título
+        title = Paragraph("Título del Informe", styles['Title'])
+        elements.append(title)
+
+        # Espacio
+        elements.append(Spacer(1, 12))
+
+        # Subtítulo
+        subtitle = Paragraph("Subtítulo del Informe", styles['Heading2'])
+        elements.append(subtitle)
+
+        # Espacio
+        
+        elements.append(Spacer(1, 12))
+
+        lore = lorem.text()+ lorem.text()+lorem.text()+ lorem.text()+lorem.text()+ lorem.text()+lorem.text()+ lorem.text()+lorem.text()+ lorem.text()
+        # Texto
+        # text = Paragraph("""Aquí va el texto del informe..               
+        #                  """, styles['BodyText'])
+        
+        
+        
+        text = Paragraph(lore, styles['BodyText']) ##texto de prueba 
+        elements.append(text)
+
+        # Espacio
+        elements.append(Spacer(1, 12))
+
+        # Imagen
+        try:
+            image_model = Casa.objects.get(numero=casa)
+        except Casa.DoesNotExist:
+            return HttpResponse('Casa no encontrada')
+        image_path = image_model.foto
+        image = Image(io.BytesIO(image_path), 200, 200)
+        elements.append(image)
+
+        # Genera el PDF
+        doc.build(elements)
+
+        # Crea una respuesta con el archivo PDF
+        buffer.seek(0)
+        return FileResponse(buffer, as_attachment=True, filename='informe.pdf')
